3f2bf7c7667a0c6fb5b5a405a54a29d87c3085ef
Luis Felipe Bardi <lombardi131341@gmail.com>
2025-05-28 19:34:43 -0300

    Add test

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,24 @@
+import { FastifyInstance } from "fastify";
+import request from 'supertest';
+import { prisma } from '@/lib/prisma';
+import { hash } from 'bcryptjs';
+
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
+    await prisma.user.create({
+        data: {
+            name: 'John Doe',
+            email: 'john.doe@example.com',
+            password_hash: await hash('123456', 6),
+            role: isAdmin ? 'ADMIN' : 'MEMBER',
+        },
+    });
+
+    const authenticateResponse = await request(app.server).post('/sessions').send({
+        email: 'john.doe@example.com',
+        password: '123456',
+    });
+
+    const { token } = authenticateResponse.body;
+
+    return { token }
+}
